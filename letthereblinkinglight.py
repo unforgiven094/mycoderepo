50e6c23efcae69a007c3b9a0d1cce29ea3971bb3
unforgiven094 <37676901+unforgiven094@users.noreply.github.com>
2018-03-24 08:10:33 +0800

    Add files via upload

---
+++ letthereblinkinglight.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+
+def Blink(numTimes,speed):
+    for i in range(0,numTimes):
+        print "Iteration " + str(i+1)
+        GPIO.output(7, True)
+        time.sleep(speed)
+        GPIO.output(7, False)
+        time.sleep(speed)
+    print "Done!"
+    GPIO.cleanup()   
+
+iterations = raw_input("Enter total number of times to blink: ")
+speed = raw_input("Enter length of each blink(seconds): ")
+
+Blink(int(iterations),float(speed)) 
+
+
+
